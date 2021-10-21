46edd2cd2ff2f924a53e53c30b082a6ed0935262
fotiemconstant <fotiemoghommahieconstant@gmail.com>
2021-10-21 12:24:38 +0100

    Added hover on twitter logo and adjusted the menu padding on hover

--- src/components/LeftSideMenu.vue
+++ src/components/LeftSideMenu.vue
@@ -2,58 +2,60 @@
   <div class="LeftSideMenu w-3/12 px-8 pt-3 border-r h-screen">
     <div>
       <!-- twitter logo -->
-      <img
-        class="w-12"
-        src="https://assets.stickpng.com/images/580b57fcd9996e24bc43c53e.png"
-        alt=""
-      />
+      <router-link to="/">
+        <img
+          class="w-14 p-1 hover:bg-lightBlue rounded-full cursor-pointer"
+          src="https://assets.stickpng.com/images/580b57fcd9996e24bc43c53e.png"
+          alt=""
+        />
+      </router-link>
       <!-- / twitter logo -->
     </div>
-    <div class="mt-6">
+    <div class="mt-2">
       <div
-        class="flex text-xl font-bold px-4 w-40 space-x-5 py-3 cursor-pointer hover:bg-gray transition delay-75 duration-200 ease-in-out rounded-full"
+        class="flex text-xl font-bold pl-4 w-40 space-x-5 py-3 cursor-pointer hover:bg-gray transition delay-75 duration-200 ease-in-out rounded-full"
       >
         <div>icon</div>
         <div>Home</div>
       </div>
       <div
-        class="flex text-xl px-4 w-40 space-x-5 py-3 cursor-pointer hover:bg-gray transition delay-75 duration-200 ease-in-out rounded-full"
+        class="flex text-xl pl-4 w-40 space-x-5 py-3 cursor-pointer hover:bg-gray transition delay-75 duration-200 ease-in-out rounded-full"
       >
         <div>icon</div>
         <div>Explore</div>
       </div>
       <div
-        class="flex text-xl px-4 w-52 space-x-5 py-3 cursor-pointer hover:bg-gray transition delay-75 duration-200 ease-in-out rounded-full"
+        class="flex text-xl pl-4 w-52 space-x-5 py-3 cursor-pointer hover:bg-gray transition delay-75 duration-200 ease-in-out rounded-full"
       >
         <div>icon</div>
         <div>Notifications</div>
       </div>
       <div
-        class="flex text-xl px-4 w-48 space-x-5 py-3 cursor-pointer hover:bg-gray transition delay-75 duration-200 ease-in-out rounded-full"
+        class="flex text-xl pl-4 w-48 space-x-5 py-3 cursor-pointer hover:bg-gray transition delay-75 duration-200 ease-in-out rounded-full"
       >
         <div>icon</div>
         <div>Messages</div>
       </div>
       <div
-        class="flex text-xl px-4 w-48 space-x-5 py-3 cursor-pointer hover:bg-gray transition delay-75 duration-200 ease-in-out rounded-full"
+        class="flex text-xl pl-4 w-48 space-x-5 py-3 cursor-pointer hover:bg-gray transition delay-75 duration-200 ease-in-out rounded-full"
       >
         <div>icon</div>
         <div>Bookmarks</div>
       </div>
       <div
-        class="flex text-xl px-4 w-32 space-x-5 py-3 cursor-pointer hover:bg-gray transition delay-75 duration-200 ease-in-out rounded-full"
+        class="flex text-xl pl-4 w-32 space-x-5 py-3 cursor-pointer hover:bg-gray transition delay-75 duration-200 ease-in-out rounded-full"
       >
         <div>icon</div>
         <div>List</div>
       </div>
       <div
-        class="flex text-xl px-4 w-40 space-x-5 py-3 cursor-pointer hover:bg-gray transition delay-75 duration-200 ease-in-out rounded-full"
+        class="flex text-xl pl-4 w-40 space-x-5 py-3 cursor-pointer hover:bg-gray transition delay-75 duration-200 ease-in-out rounded-full"
       >
         <div>icon</div>
         <div>Profile</div>
       </div>
       <div
-        class="flex text-xl px-4 w-36 space-x-5 py-3 cursor-pointer hover:bg-gray transition delay-75 duration-200 ease-in-out rounded-full"
+        class="flex text-xl pl-4 w-36 space-x-5 py-3 cursor-pointer hover:bg-gray transition delay-75 duration-200 ease-in-out rounded-full"
       >
         <div>icon</div>
         <div>More</div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,7 @@ module.exports = {
     extend: {
       backgroundColor: {
         gray: "rgba(15, 20, 25, 0.1)",
+        lightBlue: "rgba(29, 155, 240, 0.1)"
       }
     },
   },
