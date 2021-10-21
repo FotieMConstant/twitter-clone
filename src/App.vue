cd6c77d75f10b4c81c883a160ab0651742fa8766
fotiemconstant <fotiemoghommahieconstant@gmail.com>
2021-10-21 06:42:40 +0100

    Setup project init

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,11 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div>
+    <router-view/>
   </div>
 </template>
-
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
 
 <style src="./assets/tailwind.css">
